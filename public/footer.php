b7a5d4e3c6fa18a98684c57cdacbea273337296a
Barry O'Donovan <barry@opensolutions.ie>
2016-06-09 13:57:17 +0100

    Add stats collection

--- public/footer.php
+++ public/footer.php
@@ -3,7 +3,7 @@
       <div class="footer">
         <p>
           &copy; Copyright Internet Neutral Exchange Association Limited
-            (<a href="https://www.inex.ie/">INEX</a>), Dublin, Ireland 2016 
+            (<a href="https://www.inex.ie/">INEX</a>), Dublin, Ireland 2016
           
         </p>
       </div>
@@ -14,5 +14,23 @@
     <script src="/assets/jquery/dist/jquery.min.js"></script>
     <script src="/assets/bootstrap/dist/js/bootstrap.min.js"></script>
     <?php if( isset( $additionalFooter ) ) { echo $additionalFooter; } ?>
+
+    <!-- Piwik -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      _paq.push(["setDomains", ["*.ixpmanager.org","*.www.ixpmanager.org"]]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//stats.opensolutions.ie/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 13]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//stats.opensolutions.ie/piwik.php?idsite=13" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
+
   </body>
 </html>
