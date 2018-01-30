bedcde99b9c6357ffe0295bfc955791900688d2e
Nick Hilliard <nick@foobar.org>
2018-01-30 13:43:40 +0000

    Added "support, out in the open" policy

--- public/support.php
+++ public/support.php
@@ -51,6 +51,27 @@
             the issue tracker.
           </p>
 
+          <h3>"Support, Out in the Open"</h3>
+
+          <p>
+
+            INEX promotes a "Support, out in the open" model.  When support issues crop up, we invite people
+            to send them to the ixpmanager@inex.ie mailing list where a member of the community can answer
+            them.  Alternatively if there's a bug, this can be reported on the github issues tracker where it
+            can be assessed by our dev team.  When this model is used, search engines pick up relevant query
+            terms, everybody learns about the application, the developers can see what's important to the
+            community, and the community benefits as a whole.
+
+          </p>
+
+          <p>
+
+            INEX doesn't offer free private technical support.  This means that if you email one of the
+            development team directly, we will politely suggest that you send your question to the mailing
+            list.
+
+          </p>
+
         </div>
 
 <?php include 'footer.php'; ?>
