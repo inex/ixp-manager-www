cc3d21a77a62bb2f325ed3c88cf473a6c71de0ae
Barry O'Donovan <barry.odonovan@inex.ie>
2019-03-01 14:34:55 +0000

    Add APRICOT 2019 preso

--- public/presentations.php
+++ public/presentations.php
@@ -14,6 +14,26 @@
 <br>
 
 <h4>
+    APRICOT 2019 (Asia Pacific Regional Internet Conference on Operational Technologies)<br>
+    <small>Daejeon, South Korea. February 18 - 28th 2019</small><br>
+    <small>Barry O'Donovan, INEX</small><br>
+
+</h4>
+
+<ul>
+    <li>
+        IXP Route Servers with RPKI and IXP Manager
+            [<a href="media/2019/201902-apricot-rpki.pdf">pdf</a>] 
+            [<a href="https://youtu.be/cqhJwuBaxxQ?t=1549">video</a>]<br>
+            <em>In the presentation, we propose a best-practice integration of RPKI into the current 
+            IX route server context which still includes IRR support. We present the development 
+            work we have completed with IXP Manager to support RPKI and discuss our experiences 
+            at putting this live at INEX.</em>
+    </li>
+</ul>
+<br>
+
+<h4>
     APIX 19 (Asia Pacific Internet Exchange Association)<br>
     <small>Daejeon, South Korea. February 24th 2019</small><br>
     <small>Barry O'Donovan, INEX</small><br>
